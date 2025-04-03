1e11b46454b9c5f90fb8d8ce630d189fee23d3a2
kraljic1 <vk2070@g.rit.edu>
2025-04-04 01:24:59 +0200

    Add 5th image to Essential products where available

--- src/data/collections/essentialCollection.ts
+++ src/data/collections/essentialCollection.ts
@@ -3,6 +3,7 @@ import essentialAntracitMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - 
 import essentialAntracit1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - ANTRACIT/Essential antracit1.webp';
 import essentialAntracit2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - ANTRACIT/Essential antracit2.webp';
 import essentialAntracit3 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - ANTRACIT/Essential antracit3.webp';
+import essentialAntracit4 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - ANTRACIT/Essential antracit4.webp';
 import essentialOffWhiteMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - OFF-WHITE/Essential - off white.webp';
 import essentialOffWhite1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - OFF-WHITE/Essential - off white1.webp';
 import essentialOffWhite2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - OFF-WHITE/Essential - off white2.webp';
@@ -11,18 +12,22 @@ import essentialLightGreyMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL -
 import essentialLightGrey1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - LIGHT GREY/Essential - light grey1.webp';
 import essentialLightGrey2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - LIGHT GREY/Essential - light grey2.webp';
 import essentialLightGrey3 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - LIGHT GREY/Essential - light grey3.webp';
+import essentialLightGrey4 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - LIGHT GREY/Essential - light grey4.webp';
 import essentialSandMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sand/Essential - sand.webp';
 import essentialSand1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sand/Essential - sand1.webp';
 import essentialSand2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sand/Essential - sand2.webp';
 import essentialSand3 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sand/Essential - sand3.webp';
+import essentialSand4 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sand/Essential sand4.webp';
 import essentialOceanMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - Ocean/Essential ocean.webp';
 import essentialOcean1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - Ocean/Essential ocean1.webp';
 import essentialOcean2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - Ocean/Essential ocean2.webp';
 import essentialOcean3 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - Ocean/Essential ocean3.webp';
+import essentialOcean4 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - Ocean/Essential ocean4.webp';
 import essentialWhiteMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - WHITE/Essential - white.webp';
 import essentialWhite1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - WHITE/Essential - white1.webp';
 import essentialWhite2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - WHITE/Essential - white2.webp';
 import essentialWhite3 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - WHITE/Essential - white3.webp';
+import essentialWhite4 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - WHITE/Essential white4.webp';
 import essentialSandBLMain from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sandBL/Essential sandBL.webp';
 import essentialSandBL1 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sandBL/Essential sandBL1.webp';
 import essentialSandBL2 from '../../img/rollerblinds/ESSENTIAL/ESSENTIAL - sandBL/Essential sandBL2.webp';
@@ -41,7 +46,8 @@ export const essentialCollection: Product[] = [
       essentialAntracitMain,
       essentialAntracit1,
       essentialAntracit2,
-      essentialAntracit3
+      essentialAntracit3,
+      essentialAntracit4
     ],
     features: ["Light filtering"],
     colors: 5,
@@ -58,7 +64,8 @@ export const essentialCollection: Product[] = [
       essentialSandMain,
       essentialSand1,
       essentialSand2,
-      essentialSand3
+      essentialSand3,
+      essentialSand4
     ],
     features: ["Light filtering"],
     colors: 5,
@@ -93,7 +100,8 @@ export const essentialCollection: Product[] = [
       essentialOceanMain,
       essentialOcean1,
       essentialOcean2,
-      essentialOcean3
+      essentialOcean3,
+      essentialOcean4
     ],
     features: ["Light filtering"],
     colors: 5,
@@ -110,7 +118,8 @@ export const essentialCollection: Product[] = [
       essentialLightGreyMain,
       essentialLightGrey1,
       essentialLightGrey2,
-      essentialLightGrey3
+      essentialLightGrey3,
+      essentialLightGrey4
     ],
     features: ["Light filtering"],
     colors: 5,
@@ -144,7 +153,8 @@ export const essentialCollection: Product[] = [
       essentialWhiteMain,
       essentialWhite1,
       essentialWhite2,
-      essentialWhite3
+      essentialWhite3,
+      essentialWhite4
     ],
     features: ["Light filtering"],
     colors: 5,
